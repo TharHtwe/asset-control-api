135427db0d27fb8a77acb54ab7171b735d380e21
TharHtwe <49180137+TharHtwe@users.noreply.github.com>
2023-06-26 22:05:58 +0630

    Update TestController.php

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Asset;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index(Request $request)
     {
+        return response(['Assets' => Asset::all()]);
         return response([
             'message' => 'Get request success!',
         ]);
